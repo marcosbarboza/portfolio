f24dfed219b27a321a9052aedd6ca0f896cb232a
Marcos Barboza <42554923+marcosbarboza@users.noreply.github.com>
2021-02-23 20:16:54 -0300

    Update webscraping.py
    Mudança do mecanismo de envio de e-mail para o MIME ao invés do send do SMTPLIB
    Uso de HTML no body do e-mail para formatação

--- webscraping.py
+++ webscraping.py
@@ -3,58 +3,64 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 from termcolor import cprint
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
-def catchnameprice(urllink, desiredprice):
+def catchnameprice(urllink, desiredprice, destination_email):
     headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
 
     monitoring = requests.get(urllink, headers=headers)
 
     soup = BeautifulSoup(monitoring.content, 'html.parser')
     product_name = soup.find(id="productTitle").get_text()
-    catch_product_price = soup.find(id="priceblock_ourprice").get_text()
+    catch_product_price = soup.find(id="price_inside_buybox").get_text()
     pre_convert_product_price = catch_product_price[2:]
     convert_product_price = pre_convert_product_price.replace(',', '.')
-    product_price = float(convert_product_price)
+    convert_product_price_2 = convert_product_price.replace('$', '')
+    product_price = float(convert_product_price_2)
 
     print(f'Produto: {product_name.strip()}')
     print(f'Preço Atual: R${product_price}')
 
     if product_price < desiredprice:
-        subject = 'O Preco Abaixou !'.encode('ISO-8859-1')
-        body = f'Para comprar o \n{product_name.strip()}, \nAcesse: {urllink}'.encode('ISO-8859-1')
+
+        body_html = f"""<html><head></head><body><h3><span style="font-family: Verdana, Geneva, sans-serif; color: rgb(0, 168, 133);">Seu produto chegou ao pre&ccedil;o esperado:</span></h3><p><a href="{urllink}" rel="noopener noreferrer" target="_blank">Clique aqui</a> para comprar</p></body></html>"""
         cprint('Preço Atingido', 'green')
-        sendemail(subject, body)
+        sendemail(body_html,destination_email)
 
     else:
         print('--------------------------------------------')
         cprint(f'Preço não alcaçado R${product_price}', 'red')
 
 
-def sendemail(subject, body):
+def sendemail(body_html,destination_email):
+    # Server configuration
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-
     server.login('marcosjr861@gmail.com', 'mhsdwkjgyllrgget')
+    # Sending Configuration
 
-    subject = subject
-    body = body
-    msg = f"Subject: {subject}\n\n{body}".encode('ISO-8859-1')
+    msg = MIMEMultipart()
+    msg['From'] = 'marcosjr861@gmail.com'
+    msg['to'] = destination_email
+    msg['Subject'] = "Monitoramento"
+    
+    msg.attach(MIMEText(body_html, 'html'))
+    
+    # Sending
+    server.send_message(msg)
 
-    server.sendmail(
-        'marcosjr861@gmail.com',
-        'marcosjr861@gmail.com',
-        msg
-    )
     cprint('E-mail com link de compra -> Enviado', 'yellow')
     server.quit()
 
 
 url = str(input('Insira o link para monitorar: '))
 desired_price = float(input('Insira o valor desejado: '))
+destination_email = str(input('Insira seu E-mail: '))
 
 while True:
-    catchnameprice(url, desired_price)
+    catchnameprice(url, desired_price, destination_email)
     time.sleep(21600)
